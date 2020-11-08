11c449677b4db8c216fb2938eaa5748575f5d0d3
Mario Bellido <mariobellidoponce@gmail.com>
2020-11-08 19:04:57 +0100

    Add styles

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -8,33 +8,39 @@ function Autocomplete({
   onSelectSuggestion,
 }) {
   return (
-    <div>
-      <label htmlFor="autocomplete">{label}</label>
+    <div className="autocomplete">
+      <label className="autocomplete__label" htmlFor="autocomplete">
+        {label}
+      </label>
 
-      <div>
-        <input
-          id="autocomplete"
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-        />
-        {icon && <span>{icon}</span>}
+      <div className="autocomplete__outer">
+        <div className="autocomplete__inner">
+          <input
+            className="autocomplete__input"
+            id="autocomplete"
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+          />
+          {icon && <span className="autocomplete__icon">{icon}</span>}
+        </div>
+
+        {suggestions && (
+          <ul className="autocomplete__suggestions suggestions">
+            {suggestions.map((suggestion) => (
+              <li className="suggestions__item">
+                <button
+                  className="suggestions__inner"
+                  onClick={() => onSelectSuggestion(suggestion)}
+                  type="button"
+                >
+                  {suggestion}
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
-
-      {suggestions && (
-        <ul>
-          {suggestions.map((suggestion) => (
-            <li>
-              <button
-                onClick={() => onSelectSuggestion(suggestion)}
-                type="button"
-              >
-                {suggestion}
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
   );
 }
